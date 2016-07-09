aa2a586d06830afb3252dbfd24fb2b5532a985a1
urzp <urzp002@mail.ru>
2016-07-10 00:25:06 +0300

    create turn method

--- classes.rb
+++ classes.rb
@@ -3,7 +3,22 @@ class Game
     attr :board
   def initialize
     row = Array.new(7, " ")
-    @board = Array.new(6, row)
+    @board = Array.new(6){ |i|  i = Array.new(7, " ") }
+  end
+
+  def turn(player, plase)
+    marker = player.marker
+    plase = plase - 1
+    if @board.all? { |row| row[plase] = " " }
+       @board[-1][plase] = marker
+    else
+      @board.each_with_index do |row, index|
+        if row[plase] != " "
+        end
+      end
+    end
+    self.draw_board
+
   end
 
   def draw_board
@@ -13,6 +28,9 @@ class Game
       puts "|---|---|---|---|---|---|---|"
     end
   end
+private
+
+
 end
 
 class Player
