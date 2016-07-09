f2fa375b84b54b62ca0ab205cdee05d16af059e8
urzp <urzp002@mail.ru>
2016-07-09 22:45:14 +0300

    create drave method

--- classes.rb
+++ classes.rb
@@ -2,10 +2,17 @@
 class Game
     attr :board
   def initialize
-    collum = Array.new(6, " ")
-    @board = Array.new(7, collum)
+    row = Array.new(7, " ")
+    @board = Array.new(6, row)
   end
 
+  def draw_board
+    puts "  1   2   3   4   5   6   7  "
+    @board.each do |row|
+      puts "| #{row[0]} | #{row[1]} | #{row[2]} | #{row[3]} | #{row[4]} | #{row[5]} | #{row[6]} |"
+      puts "|---|---|---|---|---|---|---|"
+    end
+  end
 end
 
 class Player
