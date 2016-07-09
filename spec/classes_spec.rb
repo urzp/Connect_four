c2a0b7e06cf8415f4fb0a02838e9ee110e9ac0c0
urzp <urzp002@mail.ru>
2016-07-10 00:25:49 +0300

    create test for turn method

--- spec/classes_spec.rb
+++ spec/classes_spec.rb
@@ -7,6 +7,15 @@ describe Game do
     game = Game.new
     expect(game).to be_instance_of Game
   end
+
+
+    it "shold put \"O\"  marker in the plase 1" do
+      game = Game.new
+      player_1 = double('player')
+      allow(player_1).to receive(:marker) {"O"}
+      plase = 2
+      game.turn(player_1, plase)
+   end
 end
 
 
