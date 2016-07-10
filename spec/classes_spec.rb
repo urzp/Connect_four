2c76cf049ce7c3f8ca0a5bb54200a249d0652912
urzp <urzp002@mail.ru>
2016-07-10 22:28:36 +0300

    te4st coint toss

--- spec/classes_spec.rb
+++ spec/classes_spec.rb
@@ -4,8 +4,8 @@ require '../classes'
 
 describe Game do
   before(:each) do
-    @player_1 = double('player')
-    @player_2 = double('player')
+    @player_1 = double('Player')
+    @player_2 = double('Player')
     allow(@player_1).to receive(:marker) {"O"}
     allow(@player_2).to receive(:marker) {"X"}
     @game = Game.new(@player_1, @player_2)
@@ -15,13 +15,17 @@ describe Game do
     expect(@game).to be_instance_of Game
   end
 
-
     it "shold put \"O\"  marker in the plase 1" do
       plase = 4
       @game.turn(@player_1, plase)
       plase = 4
       @game.turn(@player_2, plase)
    end
+
+   it "Shold return player_1 or player_2" do
+     expect(@game.coin_toss.class).to eq RSpec::Mocks::Double
+   end
+
 end
 
 
