ad0b5c10046c4e527bf4bd0ab2893b04c328fd14
urzp <urzp002@mail.ru>
2016-07-10 22:07:14 +0300

    refractoring test for Game class

--- spec/classes_spec.rb
+++ spec/classes_spec.rb
@@ -3,22 +3,24 @@
 require '../classes'
 
 describe Game do
+  before(:each) do
+    @player_1 = double('player')
+    @player_2 = double('player')
+    allow(@player_1).to receive(:marker) {"O"}
+    allow(@player_2).to receive(:marker) {"X"}
+    @game = Game.new(@player_1, @player_2)
+  end
+
   it "Shold return Game class" do
-    game = Game.new
-    expect(game).to be_instance_of Game
+    expect(@game).to be_instance_of Game
   end
 
 
     it "shold put \"O\"  marker in the plase 1" do
-      game = Game.new
-      player_1 = double('player')
-      player_2 = double('player')
-      allow(player_1).to receive(:marker) {"O"}
-      allow(player_2).to receive(:marker) {"X"}
       plase = 4
-      game.turn(player_1, plase)
+      @game.turn(@player_1, plase)
       plase = 4
-      game.turn(player_2, plase)
+      @game.turn(@player_2, plase)
    end
 end
 
