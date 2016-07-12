44eda2bf2d58be2fab59ea504c39a6d0ab077c36
urzp <urzp002@mail.ru>
2016-07-12 16:56:25 +0300

    play between two players

--- tools.rb
+++ tools.rb
@@ -2,6 +2,14 @@
 
 require './classes'
 
-g = Game.new
 
-g.draw_board
+  player_1 = Player.new("x",:human)
+  player_2 = Player.new("o",:human)
+  game = Game.new(player_1,player_2)
+
+while true
+  plase = player_1.turn
+  game.turn(plase)
+  plase = player_2.turn
+  game.turn(plase)
+end
