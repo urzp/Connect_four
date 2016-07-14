3895191b845e32e326c6317de8f16f73041bf8c0
urzp <urzp002@mail.ru>
2016-07-14 22:32:29 +0300

    correction

--- tools.rb
+++ tools.rb
@@ -6,7 +6,8 @@ require './classes'
   player_1 = Player.new("x",:human)
   player_2 = Player.new("o",:human)
   game = Game.new(player_1,player_2)
-
+  game.draw_board
+  
 while !game.check_win
   plaer = game.who_turn?
   plase = plaer.turn
