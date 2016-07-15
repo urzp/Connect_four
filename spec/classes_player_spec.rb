b9565b9a22b83abf3d50efd6b0321a3f5a25ef52
urzp <urzp002@mail.ru>
2016-07-15 18:26:43 +0300

    test for find win

--- spec/classes_player_spec.rb
+++ spec/classes_player_spec.rb
@@ -2,7 +2,7 @@ require '../classes'
 
 describe Player do
   before(:each) do
-    @player_1 = Player.new("X", :human)
+    @player_1 = Player.new("x", :human)
     @player_2 = Player.new("O", :comp)
     @game = Game.new(@player_1, @player_2)
   end
@@ -14,8 +14,9 @@ describe Player do
     @game.board(2,3,"x")
     @game.draw_board
     board = @game.get_board
+    @player_1.board = board
 
-    expect(@player_1.take_y(board,2)).to eq (4)
+    expect(@player_1.take_y(2)).to eq (4)
   end
 
 
@@ -24,8 +25,9 @@ describe Player do
     @game.board(1,2,"x")
     @game.draw_board
     board = @game.get_board
+    @player_1.board = board
 
-    expect(@player_1.take_y(board,1)).to eq (3)
+    expect(@player_1.take_y(1)).to eq (3)
   end
 
   it "Shold find win line" do
@@ -34,6 +36,32 @@ describe Player do
     @game.board(3,1,"x")
     @game.draw_board
     board = @game.get_board
-    @player_1.find_win(board)
+    @player_1.board = board
+    expect(@player_1.find_win).to eq [4,1]
   end
+
+  it "Shold find win line" do
+    @game.board(1,1,"x")
+    @game.board(1,2,"x")
+    @game.board(1,3,"x")
+    @game.draw_board
+    board = @game.get_board
+    @player_1.board = board
+    expect(@player_1.find_win).to eq [1,4]
+  end
+
+  it "Shold find win line" do
+    @game.board(1,1,"x")
+    @game.board(2,2,"x")
+    @game.board(3,3,"x")
+    @game.board(4,1,"o")
+    @game.board(4,2,"o")
+    @game.board(4,3,"o")
+    @game.draw_board
+    board = @game.get_board
+    @player_1.board = board
+
+    expect(@player_1.find_win).to eq [4,4]
+  end
+
 end
