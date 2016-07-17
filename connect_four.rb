270e6782b07f9d57ebdc750a28a55b2d44e82e16
urzp <urzp002@mail.ru>
2016-07-17 21:59:40 +0300

    finsh the game

---
+++ connect_four.rb
@@ -0,0 +1,17 @@
+﻿
+
+require './classes'
+
+
+  player_1 = Player.new("x",:human)
+  player_2 = Player.new("o",:comp)
+  game = Game.new(player_1,player_2)
+  player_1.see_enimy(game.enimy?(player_1))
+  player_2.see_enimy(game.enimy?(player_2))
+  game.draw_board
+
+while !game.check_win
+  plaer = game.who_turn?
+  plase = plaer.turn(game.get_board)
+  game.turn(plase)
+end
