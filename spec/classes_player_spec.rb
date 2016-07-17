0dc46461e1c370d7070bafc294746bb6172c986a
urzp <urzp002@mail.ru>
2016-07-17 21:55:45 +0300

    add context for tests

--- spec/classes_player_spec.rb
+++ spec/classes_player_spec.rb
@@ -9,6 +9,7 @@ describe Player do
     @player_2.see_enimy(@game.enimy?(@player_2))
   end
 
+context "test take_y" do
 
   it "Should return y = 4" do
     @game.board(2,1,"x")
@@ -32,7 +33,11 @@ describe Player do
     expect(@player_1.take_y(1)).to eq (3)
   end
 
-  it "Shold find win line from [4,1]" do
+end
+
+context "test win line" do
+
+  it "Shold find win line find_line(3,\"x\")).to eq [4,1]" do
     @game.board(1,1,"x")
     @game.board(2,1,"x")
     @game.board(3,1,"x")
@@ -42,7 +47,7 @@ describe Player do
     expect(@player_1.find_line(3,"x")).to eq [4,1]
   end
 
-  it "Shold find win line from [1,4]" do
+  it "Shold find find_line(3,\"x\")).to eq [1,4]" do
     @game.board(1,1,"x")
     @game.board(1,2,"x")
     @game.board(1,3,"x")
@@ -52,7 +57,7 @@ describe Player do
     expect(@player_1.find_line(3,"x")).to eq [1,4]
   end
 
-  it "Shold find win line from [4,4]" do
+  it "Shold find find_line(3,\"x\")).to eq [4,4]" do
     @game.board(1,1,"x")
     @game.board(2,2,"x")
     @game.board(3,3,"x")
@@ -66,7 +71,7 @@ describe Player do
     expect(@player_1.find_line(3,"x")).to eq [4,4]
   end
 
-  it "Shold find win line from [5,1]" do
+  it "Shold find win line find_line(2,\"x\")).to eq [5,1]" do
     @game.board(2,1,"o")
     @game.board(4,1,"x")
     @game.board(3,1,"x")
@@ -76,14 +81,15 @@ describe Player do
     expect(@player_1.find_line(2,"x")).to eq [5,1]
   end
 
-  it "Shold find win line from [2,2]" do
-    @game.board(2,1,"o")
-    @game.board(4,1,"x")
+  it "Shold find win line find_line(3,\"x\")).to eq [1,1]" do
+    @game.board(2,1,"x")
     @game.board(3,1,"x")
+    @game.board(4,1,"x")
+
     @game.draw_board
     board = @game.get_board
     @player_1.board = board
-    expect(@player_1.find_line(1,"x")).to eq [2,2]
+    expect(@player_1.find_line(3,"x")).to eq [1,1]
   end
 
 
@@ -101,6 +107,10 @@ describe Player do
     expect(@player_1.find_win).to eq [4,4]
   end
 
+end
+
+context "test #find_blok" do
+
   it "Shold find blok line from [4,4]" do
     @game.board(1,1,"x")
     @game.board(2,2,"x")
@@ -115,7 +125,7 @@ describe Player do
     expect(@player_1.find_blok).to eq [4,4]
   end
 
-  it "Sholdn't find blok line " do
+  it "Sholdn't find blok line false" do
     @game.board(1,1,"x")
     @game.board(2,2,"x")
     @game.board(3,3,"x")
@@ -129,7 +139,7 @@ describe Player do
     expect(@player_1.find_blok).to eq false
   end
 
-  it "Shold find blok line from [4,4]" do
+  it "Shold find blok line from [2,4]" do
     @game.board(1,1,"x")
     @game.board(2,2,"x")
     @game.board(3,3,"o")
@@ -142,5 +152,39 @@ describe Player do
 
     expect(@player_1.find_blok).to eq [2,4]
   end
+
+end
+
+context "test #line_2" do
+
+  it "Shold'n find  line_2 ==> find_line_of_2).to eq false " do
+    @game.board(1,1,"o")
+    @game.board(2,1,"o")
+    @game.board(3,1,"o")
+    @game.board(5,1,"x")
+    @game.board(4,1,"x")
+    @game.board(7,1,"o")
+    @game.draw_board
+    board = @game.get_board
+    @player_1.board = board
+
+    expect(@player_1.find_line_of_2).to eq false
+  end
+
+  it "Shold find  line_2 ==> find_line_of_2).to eq [6,1] " do
+    @game.board(1,1,"o")
+    @game.board(2,1,"o")
+    @game.board(3,1,"o")
+    @game.board(5,1,"x")
+    @game.board(4,1,"x")
+
+    @game.draw_board
+    board = @game.get_board
+    @player_1.board = board
+
+    expect(@player_1.find_line_of_2).to eq [6,1]
+  end
+
+end
 
 end
